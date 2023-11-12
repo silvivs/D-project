8e5954741d9c6869297c219afad27b53d5e2761e
Johnatas Silva <johnatas.silva@icomp.ufam.edu.br>
2023-11-11 20:48:08 -0400

    implemented informed_search - johnatas

--- informed_search.py
+++ informed_search.py
@@ -12,6 +12,14 @@ def heuristic(start, goal):
     x2, y2 = goal
     return abs(x1 - x2) + abs(y1 - y2)
 
+def path(origins, current):
+    path = [current]
+    while current in origins:
+        current = origins[current]
+        path.append(current)
+    path.reverse()
+    return path
+
 def astar(graph, start, goal):
     toExplore = []
     explored = set()
@@ -30,5 +38,30 @@ def astar(graph, start, goal):
 
     while toExplore:
         current = pop_min(toExplore)[1] # Accessing the second element of the tuple (the cost estimate f_score)
-        
+
+        if current == goal:
+            return path(origins, current)
+
+        explored.add(current)
+
+        for neighbor in graph[current]:
+            if neighbor in explored:
+                continue
+
+            current_g_score = g_score[current] + graph[current][neighbor]
+
+            if current_g_score < g_score[neighbor]:
+                origins[neighbor] = current
+                g_score[neighbor] = current_g_score
+                f_score[neighbor] = g_score[neighbor] + heuristic(neighbor, goal)
+
+                if neighbor not in [node[1] for node in origins]:
+                    insert_element(origins, (f_score[neighbor], neighbor))
+    
+    return None # when there is no way to the Dogo's food :'(
+
+
+
+
+
 
